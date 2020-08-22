3c3a1c6b013d6fca2555fe237f8b4cc47e7df0dd
Tony ElHabr <anthonyelhabr@gmail.com>
2020-08-22 05:34:04 -0500

    locally rerun; try edits to main.yml

--- script.R
+++ script.R
@@ -2,7 +2,7 @@ library(tweetrmd)
 library(rtweet)
 
 lasttweet_token <- function() {
-  create_token(
+  rtweet::create_token(
     'rtweet_token',
     consumer_key = Sys.getenv('CONSUMER_KEY'),
     consumer_secret = Sys.getenv('CONSUMER_SECRET'),
@@ -13,13 +13,13 @@ lasttweet_token <- function() {
 }
 
 handle <- 'TonyElHabr'
-tweets_recent <- get_timeline(handle, n = 1000, token = lasttweet_token())
+tweets_recent <- rtweet::get_timeline(handle, n = 1000, token = lasttweet_token())
 tweets_recent_rstats_idx <- grepl('#rstats', tweets_recent$text)
 tweets_recent_rstats <- tweets_recent[tweets_recent_rstats_idx, ][1, ]
 
 tmp <- 'tweet.png'
-tweet_screenshot(
-  tweet_url(handle, tweets_recent_rstats$status_id),
+tweetrmd::tweet_screenshot(
+  tweetrmd::tweet_url(handle, tweets_recent_rstats$status_id),
   scale = 5, 
   maxwidth = 600,
   theme = 'dark',
